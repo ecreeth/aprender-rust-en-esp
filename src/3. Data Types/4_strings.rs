fb7fa41e33935e9df6791059ad00314c40715862
ecreeth <luismiguel1730@gmail.com>
2020-01-08 17:22:50 -0400

    add some 'Data Types' sections

---
+++ src/3. Data Types/4_strings.rs
@@ -0,0 +1,32 @@
+//! Strings
+
+//! Un `String` es cualquier secuencia de caracteres encerrada entre comillas dobles ("").
+
+//! Ejemplos
+
+
+//! ## Definición explícita
+
+//! El siguiente código define explícitamente la variable usando la palabra clave `&str`:
+
+fn definicion_explicita() {
+
+    // definir explícitamente
+    let lenguaje:&str = "Programación en Rust";
+
+    println!("Valor: {}", lenguaje); 
+}
+
+
+//! ## Definición implícita
+
+//! El siguiente código define implícitamente el tipo de cadena de la variable al asignarles
+//! el valor único entre comillas dobles.
+
+fn definicion_implicita() { 
+
+    // definir implícitamente
+    let lenguaje_2 = "Programación en C++";
+
+    println!("Valor: {}", lenguaje_2);
+}
