5add49e3163ffc1a82767ec6cf92e0e93af897e4
ecreeth <luismiguel1730@gmail.com>
2020-01-08 12:36:19 -0400

    add 'multiple variable assingning'

---
+++ src/2. Variables/2_multiple_assigning.rs
@@ -0,0 +1,37 @@
+//! # Asignación de múltiples variables
+
+//! Es posible asignar múltiples variables en una declaración.
+
+fn main() {
+	// Declaramos las variables `curso` y `nivel` 
+    let (curso, nivel) = ("Rust", "básica");
+
+	//! Equivalente a:
+
+	// let curso = "Rust";
+	// let nivel = "básica";
+
+	// Imprimimos las variables
+    println!("Introducción {} al lenguaje {}.", nivel, curso);
+}
+
+//! # Asignación de múltiples variables utilizando la mutibilidad
+
+fn main() {
+	// Declaramos las variables `curso` y `nivel` y las hacemos mutables 
+    let (mut curso, mut nivel) = ("Rust", "básica");
+
+	// Imprimimos las variables
+    println!("Introducción {} al lenguaje {}.", nivel, curso);
+
+    // Cambiamos su valor
+    curso = "C++";
+    nivel = "Avanzada";
+
+    // Reimprimimos las variables
+    println!("Introducción {} al lenguaje {}.", nivel, curso);
+}
+
+//! Cuando asignamos múltiples variables en un misma declaración y necesitamos
+//! mutabilidad, debemos agregar la palabra clave `mut` en aquellas variables
+//! que serán mutables. 
