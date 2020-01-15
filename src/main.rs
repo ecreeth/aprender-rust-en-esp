352d3b4e5960e086d81dd10a8f2bd1355b6594c6
ecreeth <luismiguel1730@gmail.com>
2020-01-15 11:02:39 -0400

    Remove unnesessary code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,3 @@
-#![allow(unused_must_use)]
-macro_rules! write_html {
-    ($w:expr, ) => (());
-
-    ($w:expr, $e:tt) => (write!($w, "{}", $e));
-
-    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)*) => {{
-        write!($w, "<{}>", stringify!($tag));
-        write_html!($w, $($inner)*);
-        write!($w, "</{}>", stringify!($tag));
-        write_html!($w, $($rest)*);
-    }};
-}
-
 fn main() {
-  // FIXME(#21826)
-    use std::fmt::Write;
-    let mut out = String::new();
-
-    write_html!(&mut out,
-        html[
-            head[
-            	title["Macros guide"]
-            ]
-            body[
-            	h1["Macros are the best!"]
-            	div["Hello world"]
-            ]
-        ]);
-
-    println!("{}", out);
+    println!("Luis M. Alvarado");
 }
